e52fdfe21fead250269bfb139b90600ebaae2b1b
Redoy <mdfuadulislamhridoy52@gmail.com>
2022-04-24 02:35:30 +0600

    Finish till Loop Lists

---
+++ list.py
@@ -0,0 +1,27 @@
+"""thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+print(len(thislist))"""
+
+"""names = ["Fuad", "Redoy", "MD", "Bristy", "Islam", "Riya", "Tonni", "Badol"]
+names[0:3] = ["Md", "Fuadul", "Islam"]
+names[3:5] = ["Redoy", "Bristy"]
+print(names)"""
+
+"""thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+"""
+
+"""names = ["Md", "Fuadul", "Islam"]
+names.insert(3, "Redoy")
+names.append("Bristy")
+print(names)"""
+
+# names = ["MD", "Fuadul", "MD", "Fuadul"]
+# for i in range(1, len(names)):
+#     print(i+1, " - ", names[i])
+# i = 0
+# for i in range(11):
+#     print(i)
+#     i = i + 2
+
