964b33ca70bf2b6ed2a9dceeffbbd357185b28c9
Redoy <mdfuadulislamhridoy52@gmail.com>
2022-05-02 13:57:26 +0600

    Complete Class and Object Part

---
+++ class/objects.py
@@ -0,0 +1,18 @@
+class person:
+    age = 50
+    # def __init__(self, name, age):
+    #     self.name = name
+    #     self.age = age
+    #
+    # def greeting(self):
+    #     print("Hey my name is " + self.name)
+    #     print("And my age is ", self.age, " years old.")
+
+
+p1 = person()
+p1.age = 40
+
+# p1 = person("Md Fuadul Islam Redoy", 22)
+# p1.greeting()
+
+
--- function.py
+++ function.py
@@ -1,4 +1,11 @@
-def name(f, s, th):
+"""def name(f, s, th):
     print(f + s + th)
 
 name(f = "Md ", s = "Fuadul",th = "IslamRedoy")
+"""
+
+def sum(n):
+    return 2 * n
+
+
+print(sum(122))
